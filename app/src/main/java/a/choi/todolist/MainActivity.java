af771b5d024f216fe53f4d4d24ddf2d4a75caca8
ChoiHyeongu <a11dsd123@gmail.com>
2018-04-16 23:08:24 +0900

    Add addButton design

--- app/src/main/java/a/choi/todolist/MainActivity.java
+++ app/src/main/java/a/choi/todolist/MainActivity.java
@@ -2,6 +2,8 @@ package a.choi.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckBox add = (CheckBox)findViewById(R.id.addButton);
+        add.setOnClickListener(new CheckBox.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 }
