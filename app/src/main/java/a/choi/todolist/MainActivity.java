9bcccd81d0d519aa01644bf9dbd329d49ce1a087
ChoiHyeongu <a11dsd123@gmail.com>
2018-04-18 23:18:30 +0900

    Add plan_textview.xml and add dynamically add textviews button

--- app/src/main/java/a/choi/todolist/MainActivity.java
+++ app/src/main/java/a/choi/todolist/MainActivity.java
@@ -1,9 +1,16 @@
 package a.choi.todolist;
 
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.addLayout);
+
+        Button addButton=(Button)findViewById(R.id.addButton);
+        addButton.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                ConstraintLayout plan_textVview=(ConstraintLayout)inflater.inflate(R.layout.plan_textview,null);
+                layout.addView(plan_textVview);
+            }
+        });
     }
 }
