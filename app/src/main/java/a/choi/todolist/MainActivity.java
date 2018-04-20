c2d554ba7adb5132a69517198d043d8ffabc773c
ChoiHyeongu <a11dsd123@gmail.com>
2018-04-20 23:56:47 +0900

    Refactoring add view and make error

--- app/src/main/java/a/choi/todolist/MainActivity.java
+++ app/src/main/java/a/choi/todolist/MainActivity.java
@@ -1,16 +1,12 @@
 package a.choi.todolist;
 
 import android.content.Context;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.ScrollView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final LinearLayout inLayout = (LinearLayout) findViewById(R.id.planLayout);
+        Button btn = (Button) findViewById(R.id.addButton);
+
+        btn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LinearLayout planView = (LinearLayout) inflater.inflate(R.layout.plan_textview, null);
+                inLayout.addView(planView);
+            }
+        });
     }
 }
